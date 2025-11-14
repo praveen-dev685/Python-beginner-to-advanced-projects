2e02f4c547cb06d5a865d1018398ff0d1a7bce7e
praveen-dev685 <praveen2002662@ggmail.com>
2025-11-14 19:48:38 +0530

    Number Guessing Game

--- Number Guessing Game/Number Guesing Game.py
+++ Number Guessing Game/Number Guesing Game.py
@@ -1,8 +1,9 @@
 import random
 
 print("----------------Welcome to Number guessing Game--------------")
+# it was very funny because of we create a number guessing game without int data type 
 while True:
-    value = input("Enter the Number between (1 - 100) : ")
+    value = int(input("Enter the Number between (1 - 100) : "))
     value2 = []
     for i in range(1,100):
         value2.append(i)
@@ -13,10 +14,11 @@ while True:
         print("You Won !!!")
     elif(value == ""):
         print("Please Choose any value between 1 to 100")
-    elif(len(value) > 2):
+    elif(value > 100):
         print("Your value is greater than 100 !! ")
     else:
         print("Your Choice : ",value)
         print("Computer    : ",data)
+        print("type :",type( data),type(value))
         print("Try Agaiin !!!")
         
